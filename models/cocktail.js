37727cb5571c056d0272335717d39afed47356ad
Dana Gajewska <mdgajewska@outlook.com>
2019-01-22 16:59:52 +0000

    Adds pictureUrls to temp, Updates 'garnish' to array in Cocktail model

--- models/cocktail.js
+++ models/cocktail.js
@@ -6,7 +6,7 @@ let cocktailSchema = new Schema({
   glass: { type: String },
   category: { type: String, required: true },
   ingredients: [{ ingredient: { type: Schema.Types.ObjectId, ref:'Ingredient', required: true }, unit: { type: String }, amount: { type: Number }}],
-  garnish: { type: Schema.Types.ObjectId, ref: 'Garnish' },
+  garnish: [{ type: Schema.Types.ObjectId, ref: 'Garnish' }],
   preparation: { type: String, required: true },
   pictureUrl: { type: String }
 })
